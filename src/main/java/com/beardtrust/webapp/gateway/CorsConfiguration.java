1b856c71031745ba4deceb3da2d23f3ec8457d46
Matthew Crowell <>
2021-08-02 17:57:48 -0500

    update headers

--- src/main/java/com/beardtrust/webapp/gateway/CorsConfiguration.java
+++ src/main/java/com/beardtrust/webapp/gateway/CorsConfiguration.java
@@ -20,11 +20,12 @@ import reactor.core.publisher.Mono;
  */
 @Configuration
 public class CorsConfiguration {
-	private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN";
+	private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, " +
+			"credential, X-XSRF-TOKEN, LR-Type";
 	private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
 	private static final String ALLOWED_ORIGIN = "*";
 	private static final String MAX_AGE = "3600";
-	private static final String EXPOSED_HEADERS = "Authorization, BTUID";
+	private static final String EXPOSED_HEADERS = "Authorization, BTUID, LR-Type";
 	/**
 	 * Cors filter web filter.
 	 *
